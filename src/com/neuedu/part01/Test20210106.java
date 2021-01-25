e544a3721ccadec263c7e6616854e4f4e5931174
ZHAOYANCHAO <you@example.com>
2021-01-25 14:49:44 +0800

    v1.0

---
+++ src/com/neuedu/part01/Test20210106.java
@@ -0,0 +1,10 @@
+package com.neuedu.part01;
+
+public class Test20210106 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ src/demo/HelloWorde.java
@@ -0,0 +1,11 @@
+package demo;
+/**
+ * 
+ * @author ZHAOYANCHAO
+ *
+ */
+public class HelloWorde {
+public static void main(String[] args) { 
+      System.out.println("h");
+   }
+}
